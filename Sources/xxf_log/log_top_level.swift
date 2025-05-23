7d8a763a71996dbf588da38b70761c6ac4c94dd7
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-05-23 11:33:37 +0800

    feat:api 支持回调

---
+++ Sources/xxf_log/log_top_level.swift
@@ -0,0 +1,75 @@
+import Foundation
+
+func logD(_ message: @autoclosure () -> String,
+          tag: String = "General",
+          file: String = #fileID,
+          function: String = #function,
+          line: Int = #line) {
+    LogUtils.debug("[\(tag)] [\(file):\(line) \(function)] \(message())")
+}
+
+func logD(_ message: String,
+              tag: String = "General",
+              file: String = #fileID,
+              function: String = #function,
+              line: Int = #line) {
+    LogUtils.debug("[\(tag)] [\(file):\(line) \(function)] \(message)")
+}
+
+
+
+func logI(_ message: @autoclosure () -> String,
+          tag: String = "General",
+          file: String = #fileID,
+          function: String = #function,
+          line: Int = #line) {
+    LogUtils.info("[\(tag)] [\(file):\(line) \(function)] \(message())")
+}
+
+func logI(_ message: String,
+             tag: String = "General",
+             file: String = #fileID,
+             function: String = #function,
+             line: Int = #line) {
+    LogUtils.info("[\(tag)] [\(file):\(line) \(function)] \(message)")
+}
+
+
+
+
+
+func logW(_ message: @autoclosure () -> String,
+          tag: String = "General",
+          file: String = #fileID,
+          function: String = #function,
+          line: Int = #line) {
+    LogUtils.warning("[\(tag)] [\(file):\(line) \(function)] \(message())")
+}
+
+
+func logW(_ message: String,
+                tag: String = "General",
+                file: String = #fileID,
+                function: String = #function,
+                line: Int = #line) {
+    LogUtils.warning("[\(tag)] [\(file):\(line) \(function)] \(message)")
+}
+
+
+
+
+func logE(_ message: @autoclosure () -> String,
+          tag: String = "General",
+          file: String = #fileID,
+          function: String = #function,
+          line: Int = #line) {
+    LogUtils.error("[\(tag)] [\(file):\(line) \(function)] \(message())")
+}
+
+func logE(_ message: String,
+              tag: String = "General",
+              file: String = #fileID,
+              function: String = #function,
+              line: Int = #line) {
+    LogUtils.error("[\(tag)] [\(file):\(line) \(function)] \(message)")
+}
