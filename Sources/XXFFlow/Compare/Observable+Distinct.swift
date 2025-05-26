9e472a4543e1f50ba80b339b2df568e0ddf5d7a9
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-05-26 22:38:43 +0800

    feat:拓展rxswift 对标rxjava distinct

---
+++ Sources/XXFFlow/Compare/Observable+Distinct.swift
@@ -0,0 +1,25 @@
+//
+//  Observable+Distinct.swift
+//  xxf_ios
+//
+//  Created by xxf on 2025/5/26.
+//
+
+import Dispatch
+import RxSwift
+
+public extension Observable {
+    /// 按指定字段进行去重
+    func distinctBy<T: Equatable>(by keySelector: @escaping (Element) -> T) -> Observable<Element> {
+        return distinctUntilChanged { lhs, rhs in
+            keySelector(lhs) == keySelector(rhs)
+        }
+    }
+}
+
+public extension Observable where Element: Equatable {
+    /// 去除相邻重复元素（仅适用于 Element 遵循 Equatable）
+    func distinct() -> Observable<Element> {
+        return distinctUntilChanged()
+    }
+}
