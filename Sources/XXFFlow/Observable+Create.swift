32fdc014956a9f085b14b1c02de6d067f1ff8352
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-05-26 15:57:25 +0800

    feat:支持rxswift快速创建

--- Sources/XXFFlow/Observable+Create.swift
+++ Sources/XXFFlow/Observable+Create.swift
@@ -8,7 +8,7 @@
 import Dispatch
 import RxSwift
 
-extension Observable {
+public extension Observable {
     /// 类似 RxJava 的 fromCallable：同步执行闭包并发出结果或错误
     static func fromCallable(_ callable: @escaping () throws -> Element) -> Observable<Element> {
         return Observable.deferred {
@@ -22,7 +22,7 @@ extension Observable {
 
 //    /// 把 async 函数转换成 Observable，支持抛错
 //    /// - Parameter asyncFunction: 无参数的 async 抛错函数，返回 Element
-//    static func fromAsync(
+    //   public static func fromAsync(
 //        on scheduler: SchedulerType = MainScheduler.instance,
 //        _ asyncFunction: @escaping () async throws -> Element
 //    ) -> Observable<Element> {
@@ -54,7 +54,7 @@ extension Observable {
 //    }
 }
 
-extension Observable where Element == Void {
+public extension Observable where Element == Void {
     /// fromAction 返回 Observable<Void>，执行无返回值同步代码后发出完成事件
     static func fromAction(_ action: @escaping () throws -> Void) -> Observable<Void> {
         return Observable.deferred {
