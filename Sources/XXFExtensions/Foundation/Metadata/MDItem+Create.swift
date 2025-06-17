48e804622e2795de0988895fb525584ef51e2168
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-06-17 16:27:48 +0800

    feat:add MDItem+Create.swift

---
+++ Sources/XXFExtensions/Foundation/Metadata/MDItem+Create.swift
@@ -0,0 +1,21 @@
+//
+//  MDItem+Create.swift
+//  xxf_ios
+//  增加创建拓展
+//  Created by trl on 6/17.
+//
+import CoreServices
+import Foundation
+
+extension MDItem {
+    static func create(fromPath path: String) -> MDItem? {
+        return MDItemCreate(kCFAllocatorDefault, path as CFString)
+    }
+
+    static func create(fromURL url: URL) -> MDItem? {
+        guard let cfURL = url as CFURL? else {
+            return nil
+        }
+        return MDItemCreateWithURL(kCFAllocatorDefault, cfURL)
+    }
+}
