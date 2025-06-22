78e42c3fb9e78c2d30cfb4ed39afd549da5c7b75
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-06-22 16:07:19 +0800

    feat:support api memory kb or mb functions

--- Sources/XXFExtensions/Foundation/Memorying.swift
+++ Sources/XXFExtensions/Foundation/Memorying.swift
@@ -34,6 +34,18 @@ public func currentMemoryUsage() -> UInt64 {
 }
 
 /// 测量内存占用
+/// - Parameter block: 执行单元
+/// - Returns: 内存占用 字节
+@inlinable
+@inline(__always)
+func measureMemoryUsage(of block: () -> Void) -> UInt64 {
+    let before = currentMemoryUsage()
+    block()
+    let after = currentMemoryUsage()
+    return after >= before ? after - before : 0
+}
+
+/// 测量内存占用
 /// - Parameters:
 ///   - onlyDebug: 是否只在debug模式生效
 ///   - block: 执行单元
@@ -54,14 +66,26 @@ public func measureMemoryUsage(onlyDebug: Bool = true, _ block: () -> Void) -> U
     #endif
 }
 
-/// 测量内存占用
-/// - Parameter block: 执行单元
-/// - Returns: 内存占用 字节
+/// 测量内存占用（单位：KB）
+/// - Parameters:
+///   - onlyDebug: 是否只在debug模式生效
+///   - block: 执行单元
+/// - Returns: 内存占用，单位：千字节（KB），支持小数
+@discardableResult
 @inlinable
 @inline(__always)
-func measureMemoryUsage(of block: () -> Void) -> UInt64 {
-    let before = currentMemoryUsage()
-    block()
-    let after = currentMemoryUsage()
-    return after >= before ? after - before : 0
+public func measureMemoryUsageKB(onlyDebug: Bool = true, _ block: () -> Void) -> Double {
+    Double(measureMemoryUsage(onlyDebug: onlyDebug, block)) / 1024
+}
+
+/// 测量内存占用（单位：MB）
+/// - Parameters:
+///   - onlyDebug: 是否只在debug模式生效
+///   - block: 执行单元
+/// - Returns: 内存占用，单位：兆字节（MB），支持小数
+@discardableResult
+@inlinable
+@inline(__always)
+public func measureMemoryUsageMB(onlyDebug: Bool = true, _ block: () -> Void) -> Double {
+    Double(measureMemoryUsage(onlyDebug: onlyDebug, block)) / (1024 * 1024)
 }
