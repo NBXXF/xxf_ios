6a4f26db3143e1428182a64c5062c00b59b39901
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-06-25 20:05:42 +0800

    feat:add BuiltinTable

--- Sources/XXFDatabase/Model/BaseTable.swift
+++ Sources/XXFDatabase/Model/BaseTable.swift
@@ -5,13 +5,10 @@
 //  Created by xxf on /6/4.
 //
 
-public protocol BaseTable: Codable {
+public protocol BaseTable: Codable {}
+
+/// 所有内置记录
+public protocol BuiltinTable: Codable {
     /// 返回所有内置记录（通常是静态写死）
     static var builtinRecords: [Self] { get }
-}
-
-extension BaseTable {
-    static var builtinRecords: [Self] {
-        return []
-    }
 }
