79a247ac629da37966014a43d582d7a6c7fe1e67
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-06-27 14:26:21 +0800

    feat:add empty api

---
+++ Sources/XXFExtensions/Foundation/String+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  String+Extension.swift
+//  xxf_ios
+//
+//  Created by xxf on 6/27.
+//
+
+public extension String {
+    /// 定义空字符串,避免业务代码看起来混乱
+    static var empty: String {
+        return ""
+    }
+}
