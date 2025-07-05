c6fbc98669cc7fcd19d5356b3f8cc30cf8fa1afa
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-07-05 21:38:30 +0800

    feat:lifecycle

--- Sources/XXFFlow/Lifecycle/Observable+Lifecycle.swift
+++ Sources/XXFFlow/Lifecycle/Observable+Lifecycle.swift
@@ -1,16 +1,42 @@
 //
-//  Observable+Create.swift
+//  Observable+Lifecycle.swift
 //  xxf_ios
 //
 //  Created by xxf on /5/26.
 //
+
 import ObjectiveC
 import RxCocoa
 import RxSwift
 
+#if canImport(UIKit)
+    import UIKit
+#elseif canImport(AppKit)
+    import AppKit
+#endif
+
 public extension ObservableType {
-    /// 绑定生命周期
+    /// 绑定生命周期，直到对象释放
     func bindUntilDeallocated<T: NSObject>(of object: T) -> Observable<Element> {
         return take(until: object.rx.deallocated)
     }
+
+    #if canImport(UIKit)
+        /// 绑定生命周期到 UIViewController 生命周期事件（UIKit）
+        func bindLifecycle(
+            of viewController: UIViewController,
+            untilEvent: LifecycleEvent = .viewWillDisappear
+        ) -> Observable<Element> {
+            return take(until: viewController.rx.methodInvoked(untilEvent.selector))
+        }
+
+    #elseif canImport(AppKit)
+        /// 绑定生命周期到 NSViewController 生命周期事件（AppKit）
+        func bindLifecycle(
+            of viewController: NSViewController,
+            untilEvent: LifecycleEvent = .viewWillDisappear
+        ) -> Observable<Element> {
+            return take(until: viewController.rx.methodInvoked(untilEvent.selector))
+        }
+    #endif
 }
--- Sources/XXFFlow/Lifecycle/Single+Lifecycle.swift
+++ Sources/XXFFlow/Lifecycle/Single+Lifecycle.swift
@@ -1,17 +1,45 @@
 //
-//  Observable+Create.swift
+//  Single+Lifecycle.swift
 //  xxf_ios
 //
 //  Created by xxf on /5/26.
 //
+
 import ObjectiveC
 import RxCocoa
 import RxSwift
 
+#if canImport(UIKit)
+    import UIKit
+#elseif canImport(AppKit)
+    import AppKit
+#endif
+
 public extension PrimitiveSequence where Trait == SingleTrait {
     func bindUntilDeallocated<T: NSObject>(of object: T) -> Single<Element> {
         return asObservable()
-            .take(until: object.rx.deallocated)
+            .bindUntilDeallocated(of: object)
             .asSingle()
     }
+
+    #if canImport(UIKit)
+        func bindLifecycle(
+            of viewController: UIViewController,
+            untilEvent: LifecycleEvent = .viewWillDisappear
+        ) -> Single<Element> {
+            return asObservable()
+                .bindLifecycle(of: viewController, untilEvent: untilEvent)
+                .asSingle()
+        }
+
+    #elseif canImport(AppKit)
+        func bindLifecycle(
+            of viewController: NSViewController,
+            untilEvent: LifecycleEvent = .viewWillDisappear
+        ) -> Single<Element> {
+            return asObservable()
+                .bindLifecycle(of: viewController, untilEvent: untilEvent)
+                .asSingle()
+        }
+    #endif
 }
