d0242c90ed543d2dc8e394d13dd8736a127ccf61
xxf <zhengwutati@gmail.com>
2025-11-11 16:11:52 +0800

    feat:deviceid ios 13

--- Sources/XXFFoundation/Foundation/Metadata/NSMetadataItem+Query.swift
+++ Sources/XXFFoundation/Foundation/Metadata/NSMetadataItem+Query.swift
@@ -5,10 +5,11 @@
 //  Created by xxf on 6/12.
 //
 import Foundation
-
+#if os(macOS)
 /// Spotlight 支持的字段
 extension NSMetadataItem: NSMetadataOperationProtocol {
     public func value<T>(forMetadataItem key: String) -> T? {
         return value(forAttribute: key) as? T
     }
 }
+#endif
