26fccedee1743f0458eab5054eb83913a08396f4
xxf <zhengwutati@gmail.com>
2026-04-16 14:00:27 +0800

    feat:add LifecycleCollectionViewDelegate

---
+++ Sources/XXFAdapter/LifecycleCollectionViewFlowLayoutDelegate.swift
@@ -0,0 +1,58 @@
+//
+//  LifecycleCollectionViewFlowLayoutDelegate.swift
+//  xxf_ios
+//
+//  Created by xxf
+//
+
+#if canImport(UIKit)
+open class LifecycleCollectionViewFlowLayoutDelegate: NSObject, UICollectionViewDelegateFlowLayout
+{
+    open func collectionView(_ collectionView: UICollectionView,
+                             willDisplay cell: UICollectionViewCell,
+                             forItemAt indexPath: IndexPath)
+    {
+        (cell as? LifecycleCell)?.isDisplaying = true
+    }
+
+    open func collectionView(_ collectionView: UICollectionView,
+                             didEndDisplaying cell: UICollectionViewCell,
+                             forItemAt indexPath: IndexPath)
+    {
+        (cell as? LifecycleCell)?.isDisplaying = false
+    }
+
+    open func collectionView(_ collectionView: UICollectionView,
+                             shouldSelectItemAt indexPath: IndexPath) -> Bool
+    {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? LifecycleCell
+        else
+        {
+            return true
+        }
+        return cell.cellShouldSelect(at: indexPath)
+    }
+
+    open func collectionView(_ collectionView: UICollectionView,
+                             shouldDeselectItemAt indexPath: IndexPath) -> Bool
+    {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? LifecycleCell
+        else
+        {
+            return true
+        }
+        return cell.cellShouldDeselect(at: indexPath)
+    }
+
+    open func collectionView(_ collectionView: UICollectionView,
+                             shouldHighlightItemAt indexPath: IndexPath) -> Bool
+    {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? LifecycleCell
+        else
+        {
+            return true
+        }
+        return cell.cellShouldHighlight(at: indexPath)
+    }
+}
+#endif
