c94caa906916f2f92f26543920665620bed55050
xxf <zhengwutati@gmail.com>
2026-05-08 19:42:04 +0800

    feat:UniqueItem custom id

--- Sources/XXFAdapter/DiffableDataSource/UniqueItem.swift
+++ Sources/XXFAdapter/DiffableDataSource/UniqueItem.swift
@@ -8,25 +8,34 @@
 import Foundation
 
 public final class UniqueItem<Value: Hashable>: Hashable, @unchecked Sendable {
-    public let id: UUID
+    public let id: String
     public var value: Value
 
-    public init(value: Value) {
-        self.id = UUID()
+    public init(id: String = UUID().uuidString, value: Value) {
+        self.id = id
         self.value = value
     }
 
-    public static func wrap(_ items: [Value]) -> [UniqueItem] {
-        items.map { UniqueItem(value: $0) }
+    /// 用随机id 来标识
+    public static func wrap(_ items: [Value]) -> [UniqueItem<Value>] {
+        items.map { UniqueItem<Value>(value: $0) }
+    }
+
+    /// 用index位置来标识
+    public static func wrapWithIndex(_ items: [Value]) -> [UniqueItem<Value>] {
+        items.enumerated().map { index, item in
+            UniqueItem<Value>(id: String(index), value: item)
+        }
     }
 
     // MARK: - Hashable
 
     public func hash(into hasher: inout Hasher) {
         hasher.combine(id)
+        hasher.combine(value)
     }
 
     public static func == (lhs: UniqueItem, rhs: UniqueItem) -> Bool {
-        lhs.id == rhs.id
+        lhs.id == rhs.id && lhs.value == rhs.value
     }
 }
