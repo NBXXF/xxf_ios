644ac6a31163f44947444d9d5010fe6bbb6e90ae
xxf <zhengwutati@gmail.com>
2026-05-13 15:19:09 +0800

    fix:bridge codec

---
+++ Sources/XXFUIKit/Web/Bridge/Model/WebEventResponse.swift
@@ -0,0 +1,40 @@
+//
+//  WebEventResponse.swift
+//  xxf_ios
+//  h5->native event body
+//  Created by xxf on 5/13.
+//
+import Foundation
+import XXFJson
+
+public struct WebEventResponse<T: Codable>: Codable {
+    public let code: Int
+    public let message: String?
+    public let data: T
+
+    public static func success(data: T, message: String? = "ok") -> WebEventResponse<T> {
+        WebEventResponse(code: 200, message: message, data: data)
+    }
+
+    public static func fail(data: T, message: String? = "fail", code: Int = -1024) -> WebEventResponse<T> {
+        WebEventResponse(code: code, message: message, data: data)
+    }
+}
+
+public extension WebEventResponse where T == AnyCodable {
+    static func webviewReleased() -> WebEventResponse<AnyCodable> {
+        WebEventResponse<AnyCodable>(
+            code: -1,
+            message: "BridgeWebView has been released",
+            data: AnyCodable(NSNull())
+        )
+    }
+
+    static func webviewOnWebEventNotSet() -> WebEventResponse<AnyCodable> {
+        WebEventResponse<AnyCodable>(
+            code: -2,
+            message: "onWebEvent is not set",
+            data: AnyCodable(NSNull())
+        )
+    }
+}
