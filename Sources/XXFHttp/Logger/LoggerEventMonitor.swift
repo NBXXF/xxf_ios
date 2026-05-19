5cac3817847c04081eb3527fdf0477e48a6818fd
xxf <zhengwutati@gmail.com>
2026-05-19 19:43:07 +0800

    fix:Attempting to resolve crash  currentRequest: task.currentRequest.map(Request.init)

--- Sources/XXFHttp/Logger/LoggerEventMonitor.swift
+++ Sources/XXFHttp/Logger/LoggerEventMonitor.swift
@@ -9,10 +9,6 @@ import Foundation
 import Pulse
 
 open class LoggerEventMonitor: EventMonitor, @unchecked Sendable {
-    /// 更新打印发起请求的header key, request.allHTTPHeaderFields 部分机型会闪退
-    public static func setIncludeRequestHeaderKeys(_ keys: Set<String>) {
-        NetworkLogger.includeHeaderKeys = keys
-    }
 
     // MARK: - Singleton
 
