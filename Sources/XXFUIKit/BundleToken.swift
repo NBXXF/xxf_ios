e6be7a0e51a6b800c4e7640318bb717360370e95
xxf <zhengwutati@gmail.com>
2026-06-04 15:15:47 +0800

    feat:Add BundleToken

--- Sources/XXFUIKit/BundleToken.swift
+++ Sources/XXFUIKit/BundleToken.swift
@@ -7,12 +7,26 @@
 
 import Foundation
 
-public final class BundleToken {
-    public static let bundle: Bundle = {
-        #if SWIFT_PACKAGE
-        return Bundle.module
-        #else
-        return Bundle(for: BundleToken.self)
-        #endif
-    }()
-}
+/// 方式1 Bundle.module 性能更好
+//
+// public final class BundleToken {
+//    public static let bundle: Bundle = {
+//        /**
+//         必须配置process ,其中"Resources" 是目录
+//         .target(
+//             name: "MouduleName",
+//             resources: [
+//                 .process("Resources")
+//             ]
+//         )
+//         */
+//        #if SWIFT_PACKAGE
+//        return Bundle.module
+//        #else
+//        return Bundle(for: BundleToken.self)
+//        #endif
+//    }()
+// }
+
+/// 方式2: 每个模块写一个BundleToken空类,业务自己 Bundle(for: BundleToken.self)
+public final class BundleToken {}
